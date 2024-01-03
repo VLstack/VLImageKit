b789e0e188e7a53d7a34763c6fc8303f7cf83e1c
VLstack <github@vlstack.fr>
2024-01-03 13:57:48 +0100

    import from Yumeo

--- Sources/VLImageKit/VLImageKit.swift
+++ Sources/VLImageKit/VLImageKit.swift
@@ -1,2 +1,37 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+import Foundation
+import SwiftUI
+#if canImport(UIKit)
+import UIKit
+#elseif canImport(AppKit)
+import AppKit
+#endif
+
+extension Image
+{
+ /// Initializes a SwiftUI `Image` from data.
+ init(_ data: Data,
+      systemDefault: String = "photo")
+ {
+  #if canImport(UIKit)
+  if let uiImage = UIImage(data: data)
+  {
+   self.init(uiImage: uiImage)
+  }
+  else
+  {
+   self.init(systemName: systemDefault)
+  }
+  #elseif canImport(AppKit)
+  if let nsImage = NSImage(data: data)
+  {
+   self.init(nsImage: nsImage)
+  }
+  else
+  {
+   self.init(systemName: systemDefault)
+  }
+  #else
+  self.init(systemName: systemDefault)
+  #endif
+ }
+}
